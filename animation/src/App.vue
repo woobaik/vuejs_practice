90bcd70ce91aaccf96265651b6eacd84c7b5128f
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-10 22:45:58 -0700

    change the board for practicing

--- animation/src/App.vue
+++ animation/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <hello-world></hello-world>
-  </div>
+
 </template>
 
 <script>
@@ -10,18 +8,13 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
