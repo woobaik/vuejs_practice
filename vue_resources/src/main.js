be6eb80f39b5d47bee77fa4a6c97e2c6062fb124
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-07-29 20:51:12 -0700

    Add vue-resources and fake db

--- vue_resources/src/main.js
+++ vue_resources/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import Header from './components/Header.vue';
 
+
+Vue.component('app-header', Header)
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
+
 
 new Vue({
   render: h => h(App),
