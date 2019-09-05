6912eed4fabeb14ee8b2f99e7b66aace574427b4
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-09-04 19:54:27 -0700

    Add styling for chatroom

--- realtime-chat/src/components/Home.vue
+++ realtime-chat/src/components/Home.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="home">
-    Home component
-  </div>    
+    <div class="container">
+      <div class="col s10 chatroom">
+        <div class="card">
+          <span class="card-title">SAMPLE CHAT ROOM</span>
+          <div class="chat-body">
+            <div class="chat-info">
+              <div class="chat-user">Name</div>
+              <div class="chat-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum veniam eveniet, dignissimos modi aspernatur. Expedita cupiditate non dolore sit harum minus cum, odit libero optio velit aperiam, repudiandae error.</div>
+            </div>
+              <div class="chat-info">
+                <div class="chat-user">Joungwoo Baik</div>
+              <div class="chat-message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt rerum veniam eveniet, dignissimos modi aspernatur. Expedita cupiditate non dolore sit harum minus cum, odit libero optio velit aperiam, repudiandae error.</div>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,5 +30,46 @@ export default {
 <style scoped>
   .home {
     margin-top: 60px;
+    min-height: 100vh;
   }
+
+  .chatroom {
+    min-height: 80vh;
+  }
+
+  .card {
+    background-color: light-grey;
+  }
+
+  .chat-body {
+    background-color: #fff5f5;
+    height: 70vh;
+    text-align: left;
+    padding: 1rem 0.5rem 0 1rem;
+    color: white;
+  }
+
+  .chat-info {
+    display: flex;
+    flex-basis: 10%;
+    margin-bottom: 1rem;
+  }
+
+  .chat-user {
+    display: inline-block;
+    color: #733c3c;
+    flex-basis: 8rem;
+    
+    
+  }
+
+  .chat-message {
+    display: inline-block;
+    padding-left: 1rem;
+    color: #1b471b;
+    max-width: 85%;
+    flex-basis: auto;
+    
+  }
+  
 </style>
